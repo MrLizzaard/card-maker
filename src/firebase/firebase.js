366d3228aaba33a382104d8f641c9850a5e10a69
Do Hyeong Kim <dhk21512@gmail.com>
2021-08-10 11:04:42 +0900

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,25 @@
 import firebase from "firebase";
+
+const API = process.env.REACT_APP_FIREBASE_API_KEY;
+
 let config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: API,
+
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+
   projectId: "business-card-maker-7ff0f",
+
   storageBucket: "business-card-maker-7ff0f.appspot.com",
+
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSANGE_SENDER,
+
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(config);
+
 export default firebaseApp;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "@fortawesome/fontawesome-free/js/all.js";
 import "./index.module.css";
-import App from "./app";
+// import App from "./app";
 import AuthService from "./firebase/auth";
+import Login from "./components/login";
 
 const authService = new AuthService();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App authServise={authService} />
+    <Login authService={authService} />
   </React.StrictMode>,
   document.getElementById("root")
 );
