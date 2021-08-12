34ff5488242e851f430534c5ee372b317d3718d0
Do Hyeong Kim <dhk21512@gmail.com>
2021-08-12 17:31:39 +0900

    data

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import Editor from "../editor/editor";
 import Footer from "../footer/footer";
@@ -7,6 +7,42 @@ import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 
 const Maker = ({ authService }) => {
+  const [card, setCard] = useState([
+    {
+      id: "1",
+      name: "ellie",
+      company: "Samsung",
+      theme: "light",
+      title: "Software Engineer",
+      email: "dhk215@naver.com",
+      message: "hello!",
+      fileName: "ellie",
+      fileURL: "ellie.png",
+    },
+    {
+      id: "2",
+      name: "kdh",
+      company: "kakao",
+      theme: "light",
+      title: "Engineer",
+      email: "dhk215@naver.com",
+      message: "hello!",
+      fileName: "kdh",
+      fileURL: "kdh.png",
+    },
+    {
+      id: "3",
+      name: "aaa",
+      company: "carrot",
+      theme: "light",
+      title: "Software",
+      email: "dhk215@naver.com",
+      message: "hello!",
+      fileName: "aaa",
+      fileURL: "aaa.png",
+    },
+  ]);
+
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -19,12 +55,13 @@ const Maker = ({ authService }) => {
       }
     });
   });
+
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor data={card} />
+        <Preview data={card} />
       </div>
       <Footer />
     </section>
--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./preview.module.css";
 
-const Preview = (props) => (
-  <section className={styles.preview}>
-    <h1 className={styles.title}>Card Preview</h1>
-  </section>
-);
+const Preview = ({ data }) => {
+  return (
+    <section className={styles.preview}>
+      <h1 className={styles.title}>Card Preview</h1>
+    </section>
+  );
+};
 
 export default Preview;
